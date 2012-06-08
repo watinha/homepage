08533367215af5e0e01f01e44d38cda12466a17c
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2012-06-08 18:34:02 -0300

    skipping open items

--- js/files/keyboard_handler.js
+++ js/files/keyboard_handler.js
@@ -3,13 +3,22 @@ var KeyboardHandler = function (params) {
         private = {main_elements: params.main_elements};
 
     public.move_down = function (active_element) {
-        var current_index = private.find_active(active_element);
-        private.main_elements[(current_index + 1) % private.main_elements.length].focus();
+        var current_index = private.find_active(active_element),
+            next_index = (current_index + 1) % private.main_elements.length;
+        if (private.main_elements[next_index].className &&
+            Helpers.hasClass(private.main_elements[next_index], "open"))
+            next_index = (current_index + 2) % private.main_elements.length;
+        private.main_elements[next_index].focus();
     };
 
     public.move_up = function (active_element) {
-        var current_index = private.find_active(active_element) - 1;
-        private.main_elements[(current_index >= 0 ? current_index : (private.main_elements.length - 1))].focus();
+        var current_index = private.find_active(active_element) - 1,
+            next_index = (current_index >= 0 ? current_index : (private.main_elements.length - 1));
+        if (private.main_elements[next_index].className &&
+            Helpers.hasClass(private.main_elements[next_index], "open"))
+            next_index = ((current_index - 1) >= 0 ?
+                (current_index - 1) : (private.main_elements.length - 1));
+        private.main_elements[next_index].focus();
     };
 
     private.find_active = function (active_element) {
--- tests/unit/test_keyboard_handler.js
+++ tests/unit/test_keyboard_handler.js
@@ -24,6 +24,31 @@ describe('KeyboardHandler class suite', function () {
         };
     });
 
+    it('navigate should go to next focusable element skipping opened element', function () {
+        var elements = [], i,
+            handler, active_element,
+            expected = [0, 0, 0, 0, 1];
+
+        for (i = 0; i < 5; i++) {
+            elements[i] = {
+                focus_count: 0,
+                focus: function () {
+                    this.focus_count = 1;
+                }
+            };
+        };
+        active_element = elements[2];
+        elements[3].className = "some classes open"; // skip open class it must (Yoda)
+        handler = KeyboardHandler({
+            main_elements: elements
+        });
+        handler.move_down(active_element);
+
+        for (i = 0; i < 5; i++) {
+            expect(elements[i].focus_count).toBe(expected[i]);
+        };
+    });
+
     it('navigate should go to previous focusable element', function () {
         var elements = [], i,
             handler, active_element,
@@ -38,6 +63,31 @@ describe('KeyboardHandler class suite', function () {
             };
         };
         active_element = elements[2];
+        handler = KeyboardHandler({
+            main_elements: elements
+        });
+        handler.move_up(active_element);
+
+        for (i = 0; i < 5; i++) {
+            expect(elements[i].focus_count).toBe(expected[i]);
+        };
+    });
+
+    it('navigate should go to previous focusable element skipping opened element', function () {
+        var elements = [], i,
+            handler, active_element,
+            expected = [0, 0, 1, 0, 0];
+
+        for (i = 0; i < 5; i++) {
+            elements[i] = {
+                focus_count: 0,
+                focus: function () {
+                    this.focus_count = 1;
+                }
+            };
+        };
+        active_element = elements[4];
+        elements[3].className = "some classes open"; // skip open class it must (Yoda)
         handler = KeyboardHandler({
             main_elements: elements
         });
