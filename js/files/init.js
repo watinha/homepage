89fc2d3e837a0db13c6732ebc2b220b03f1ef223
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2012-06-08 16:47:36 -0300

    separates things in init.js

--- js/files/init.js
+++ js/files/init.js
@@ -1,56 +1,75 @@
 (function () {
     window.onload = function () {
         if (document.querySelectorAll) {
-            var first_line = document.querySelector("body > div.header+hr"),
-                base_elements = document.querySelectorAll("body > div:not(.header)"),
-                diagonal = DiagonalView({
-                    main_elements: base_elements
-                }),
-                standard = StandardView({
-                    base_elements: base_elements,
-                    first_line: first_line
-                }),
-                controller = ViewController({
-                    body_element: document.body,
-                    first_line: first_line,
-                    layout_objs: {
-                        'standard': standard,
-                        'diagonal': diagonal
-                    },
-                    click_handler: diagonal
-                }),
-                keyboard_handler = KeyboardHandler({
-                    main_elements: base_elements
-                });
-            controller.init();
-            controller.render('standard');
+            var App = (function () {
+                var build_view_controller, setup_event_listeners, init, // methods
+                    first_line = document.querySelector("body > div.header+hr"),
+                    base_elements = document.querySelectorAll("body > div:not(.header)"),
+                    controller = null;
 
-            /**
-              * Setting up events that the application will respond to
-              */
-            for (var i = 0; i < base_elements.length; i++) {
-                Helpers.activateListener(base_elements[i], controller.click);
-            };
-            document.body.addEventListener("keydown", function (ev) {
-                var index;
-                switch (ev.keyCode) {
-                    case 27:
-                        controller.render('standard');
-                        break;
-                    case 40:
-                        keyboard_handler.move_down(document.activeElement);
-                        break;
-                    case 39:
-                        keyboard_handler.move_down(document.activeElement);
-                        break;
-                    case 38:
-                        keyboard_handler.move_up(document.activeElement);
-                        break;
-                    case 37:
-                        keyboard_handler.move_up(document.activeElement);
-                        break;
-                }
-            }, true);
+                build_view_controller = function () {
+                    var diagonal = DiagonalView({
+                            main_elements: base_elements
+                        }),
+                        standard = StandardView({
+                            base_elements: base_elements,
+                            first_line: first_line
+                        });
+
+                    controller = ViewController({
+                        body_element: document.body,
+                        first_line: first_line,
+                        layout_objs: {
+                            'standard': standard,
+                            'diagonal': diagonal
+                        },
+                        click_handler: diagonal
+                    });
+                    controller.init();
+                    controller.render('standard');
+                };
+
+                setup_event_listeners = function () {
+                    var keyboard_handler = KeyboardHandler({
+                            main_elements: base_elements
+                        });
+                    /**
+                      * Setting up events that the application will respond to
+                      */
+                    for (var i = 0; i < base_elements.length; i++) {
+                        Helpers.activateListener(base_elements[i], controller.click);
+                    };
+                    document.body.addEventListener("keydown", function (ev) {
+                        var index;
+                        switch (ev.keyCode) {
+                            case 27:
+                                controller.render('standard');
+                                break;
+                            case 40:
+                                keyboard_handler.move_down(document.activeElement);
+                                break;
+                            case 39:
+                                keyboard_handler.move_down(document.activeElement);
+                                break;
+                            case 38:
+                                keyboard_handler.move_up(document.activeElement);
+                                break;
+                            case 37:
+                                keyboard_handler.move_up(document.activeElement);
+                                break;
+                        }
+                    }, true);
+                };
+
+                init = function () {
+                    build_view_controller();
+                    setup_event_listeners();
+                };
+
+                return {
+                    init: init
+                };
+            })().init();
         }
     }
 })();
