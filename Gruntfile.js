96399ef1959f022660dfe2f77dbd8ecdf99b7398
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2015-11-16 00:05:49 -0200

    complexity report

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,6 +51,24 @@ module.exports = function (grunt) {
                     { src: 'http://wwatana.be', }
                 ]
             }
+        },
+        complexity: {
+            generic: {
+                src: ['js/**/*.js'],
+                exclude: ['js/vendor/modernizr.js'],
+                options: {
+                    breakOnErrors: true,
+                    jsLintXML: 'report.xml',         // create XML JSLint-like report
+                    checkstyleXML: 'checkstyle.xml', // create checkstyle report
+                    pmdXML: 'pmd.xml',               // create pmd report
+                    errorsOnly: false,               // show only maintainability errors
+                    cyclomatic: [3, 7, 12],          // or optionally a single value, like 3
+                    halstead: [8, 13, 20],           // or optionally a single value, like 8
+                    maintainability: 100,
+                    hideComplexFunctions: false,     // only display maintainability
+                    broadcast: false                 // broadcast data over event-bus
+                }
+            }
         }
     });
 
